ca2bd70ee4f5348014e79d60f83f6b3c2b1da752
Alexander <alex.gl.cpp@gmail.com>
2015-02-12 21:26:29 +0100

    testing the player

--- core/src/net/isuky/dawnless/levels/Playground.java
+++ core/src/net/isuky/dawnless/levels/Playground.java
@@ -9,38 +9,42 @@ package net.isuky.dawnless.levels;
 import com.badlogic.gdx.Game;
 
 import net.isuky.dawnless.level.Level;
+import net.isuky.dawnless.player.Player;
 
 public class Playground implements Level
 {
+	public Player player;
+	
 
 	@Override
 	public void initialize(Game game) 
 	{
-
+		player = new Player(10, 10);
+		player.initialize();
 	}
 
 	@Override
 	public void loadContent()
 	{
-		
+		player.loadContent();
 	}
 
 	@Override
 	public void dispose() 
 	{
-		
+		player.dispose();
 	}
 
 	@Override
 	public void update(float delta) 
 	{
-		
+		player.update(delta);
 	}
 
 	@Override
 	public void render(float delta)
 	{
-		
+		player.render(delta);
 	}
 
 }
