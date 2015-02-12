19c9e95c681058b9f160a4318f3970ba535a805a
Alexander <alex.gl.cpp@gmail.com>
2015-02-12 18:32:27 +0100

    commenting

--- core/src/net/isuky/heroic/player/Player.java
+++ core/src/net/isuky/heroic/player/Player.java
@@ -23,42 +23,65 @@ public class Player
 		this.playerVec = playerVec;
 	}
 	
-	
+	/**
+	 * Initializes the Player
+	 * */
 	public void initialize()
 	{
 		
 	}
 	
+	/**
+	 * Loads all the Player Content(Textures, Sounds etc..)
+	 * */
 	public void loadContent()
 	{
 		//load tex here
 	}
 	
+	/**
+	 * Disposes all the Content of the Player
+	 * */
 	public void dispose()
 	{
 		
 	}
 	
+	/**
+	 * Updates the Player
+	 * */
 	public void update(float delta)
 	{
 		
 	}
 	
+	/**
+	 * Renders the Player
+	 * */
 	public void render(float delta)
 	{
 		
 	}
 	
+	/**
+	 * Gets the Position of the Player on the X-Axis
+	 **/
 	public float getPosX()
 	{
 		return x;
 	}
 	
+	/**
+	 * Gets the Position of the Player on the Y-Axis
+	 **/	
 	public float getPosY()
 	{
 		return y;
 	}
 	
+	/**
+	 * Gets the Position of the Player as a Vector2 (com.badlogic.gdx.math.Vector2)
+	 **/
 	public Vector2 getPosition()
 	{
 		return playerVec;
