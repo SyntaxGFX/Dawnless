4950ca58aa96ebbe50b3397e2944ff5c765f41bd
Alexander <alex.gl.cpp@gmail.com>
2015-02-12 18:25:40 +0100

    setting up player class

--- core/src/net/isuky/heroic/player/Player.java
+++ core/src/net/isuky/heroic/player/Player.java
@@ -1,10 +1,26 @@
 package net.isuky.heroic.player;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+
 public class Player 
 {
-	public Player()
+	
+	private float x,y;
+	private Vector2 playerVec;
+	private Texture playerTex;
+	private Sprite playerSpr;
+	
+	public Player(float x, float y)
 	{
-		
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Player(Vector2 playerVec)
+	{
+		this.playerVec = playerVec;
 	}
 	
 	
@@ -15,7 +31,7 @@ public class Player
 	
 	public void loadContent()
 	{
-		
+		//load tex here
 	}
 	
 	public void dispose()
@@ -31,5 +47,20 @@ public class Player
 	public void render(float delta)
 	{
 		
+	}
+	
+	public float getPosX()
+	{
+		return x;
+	}
+	
+	public float getPosY()
+	{
+		return y;
+	}
+	
+	public Vector2 getPosition()
+	{
+		return playerVec;
 	}
 }
